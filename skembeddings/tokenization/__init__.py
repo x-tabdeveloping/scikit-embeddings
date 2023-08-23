356470c73a1f1c2ac8e1fb6d9ae1fe09c06dfc5f
Márton Kardos <power.up1163@gmail.com>
2023-08-23 15:54:41 +0200

    Added __all__ to tokenization init

--- skembeddings/tokenization/__init__.py
+++ skembeddings/tokenization/__init__.py
@@ -2,3 +2,10 @@ from skembeddings.tokenization.dummy import DummyTokenizer
 from skembeddings.tokenization.filter import TokenFilter
 from skembeddings.tokenization.huggingface import HuggingFaceTokenizer
 from skembeddings.tokenization.spacy import SpacyTokenizer
+
+__all__ = [
+    "DummyTokenizer",
+    "TokenFilter",
+    "HuggingFaceTokenizer",
+    "SpacyTokenizer",
+]

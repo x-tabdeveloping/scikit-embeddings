2226b3b4a47297d6713562777694f8f9469438f1
Márton Kardos <power.up1163@gmail.com>
2023-09-03 19:52:59 +0200

    Fixed registry issue

--- skembeddings/__init__.py
+++ skembeddings/__init__.py
@@ -1,9 +1,8 @@
 import catalogue
 from confection import Config, registry
 
-from skembeddings import models, tokenizers
-
 registry.tokenizers = catalogue.create(
     "confection", "tokenizers", entry_points=False
 )
 registry.models = catalogue.create("confection", "models", entry_points=False)
+from skembeddings import models, tokenizers

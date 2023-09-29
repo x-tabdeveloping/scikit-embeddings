c834dd5d1d1d556f4dae5e89df0501aedea958ff
Márton Kardos <power.up1163@gmail.com>
2023-09-29 09:30:52 +0200

    Gensim is no longer optional

--- skembeddings/models/__init__.py
+++ skembeddings/models/__init__.py
@@ -3,16 +3,8 @@ from typing import Literal
 from confection import registry
 
 from skembeddings.error import NotInstalled
-
-try:
-    from skembeddings.models.word2vec import Word2VecEmbedding
-except ModuleNotFoundError:
-    Word2VecEmbedding = NotInstalled("Word2VecEmbedding", "gensim")
-
-try:
-    from skembeddings.models.doc2vec import ParagraphEmbedding
-except ModuleNotFoundError:
-    ParagraphEmbedding = NotInstalled("ParagraphEmbedding", "gensim")
+from skembeddings.models.doc2vec import ParagraphEmbedding
+from skembeddings.models.word2vec import Word2VecEmbedding
 
 try:
     from skembeddings.models.glove import GloVeEmbedding
